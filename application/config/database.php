ebf742d5650f5c2dbe7820747a5e31946a9460f7
James Pollard <james.pollard@juicymedia.co.uk>
2023-10-03 16:44:48 +0100

    Add mongodb

--- application/config/database.php
+++ application/config/database.php
@@ -93,6 +93,17 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('MONGODB_HOST', 'mongo'),
+            'port' => env('MONGODB_PORT', 27017),
+            'database' => env('MONGODB_DATABASE', 'database'),
+            'username' => env('MONGODB_USERNAME', ''),
+            'password' => env('MONGODB_PASSWORD', ''),
+            'options' => [
+                'database' => 'admin'
+            ]
+        ],
     ],
 
     /*
--- application/routes/api.php
+++ application/routes/api.php
@@ -13,8 +13,17 @@
 
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TestController;
 
 Route::group(['middleware' => ['auth:api']], function () {
     Route::apiResource('user', UserController::class);
     Route::apiResource('role', RoleController::class)->only(['index']);
+});
+
+Route::prefix('test')->group(function() {
+    Route::get('/', [TestController::class, 'index']);
+    Route::post('/', [TestController::class, 'store']);
+    Route::get('/{id}', [TestController::class, 'show']);
+    Route::put('/{id}', [TestController::class, 'update']);
+    Route::delete('/{id}', [TestController::class, 'destroy']);
 });
