8f0c2bc7c924f1d1e38ea1889adc683ae263aa83
James Pollard <james.pollard@juicymedia.co.uk>
2023-10-26 18:44:43 +0100

    Split CSV (WIP)

--- application/import.php
+++ application/import.php
@@ -2,8 +2,7 @@
 
 class CsvReader
 {
-
-    public static function readCsv($csvFile, $maxEnquiries = 999999) : array
+    public static function execute($csvFile, $maxEnquiries = 999999) : array
     {
         $handle = fopen($csvFile, 'r');
         $headers = fgetcsv($handle);
@@ -71,9 +70,108 @@ class CsvReader
     }
 }
 
+class CsvSplitter {
+    const OUTPUT_DIR = "data/output";
+    public static function execute($filePath, $enquiriesPerFile = 1000, $maxFileCount = 3) : void
+    {
+        $handle = self::openCsv($filePath);
+        $headers = fgetcsv($handle);
+        self::ensureOutputDirectoryExists();
+
+        $fileCount = $enquiryCount = 0;
+        $currentFile = null;
+
+        while (!feof($handle)) {
+            $row = fgetcsv($handle);
+
+            if (self::isNewEnquiry($row, $headers)) {
+                $enquiryCount++;
+
+                if (self::shouldSwitchFile($currentFile, $enquiryCount, $enquiriesPerFile)) {
+                    $fileCount++;
+                    if ($fileCount > $maxFileCount) {
+                        break;
+                    }
+
+                    $currentFile = self::switchFile($currentFile, $fileCount, $headers);
+                    $enquiryCount = 1;
+                }
+            }
+
+            if ($row) {
+                fputcsv($currentFile, $row);
+            }
+        }
+
+        self::closeFile($currentFile);
+        fclose($handle);
+    }
+
+    // todo return type of resource?
+    private static function openCsv($filePath)
+    {
+        $handle = fopen($filePath, 'r');
+        if (!$handle)  {
+            throw new \RuntimeException("Failed to open source file: {$filePath}");
+        }
+
+        return $handle;
+    }
+
+    private static function ensureOutputDirectoryExists() : void
+    {
+        if (!is_dir(self::OUTPUT_DIR)) {
+            mkdir(self::OUTPUT_DIR, 0777, true);
+        }
+    }
+
+    private static function isNewEnquiry($row, $headers) : bool
+    {
+        $keyIndex = array_search('Key', $headers);
+        return !empty($row[$keyIndex]);
+    }
+
+    private static function shouldSwitchFile($currentFile, $enquiryCount, $enquiriesPerFile) : bool
+    {
+        return $currentFile === null || $enquiryCount > $enquiriesPerFile;
+    }
+
+    private static function switchFile($currentFile, $fileCount, $headers)
+    {
+        if (is_resource($currentFile)) {
+            fclose($currentFile);
+        }
+        return self::createNewFile($fileCount, $headers);
+    }
+
+    private static function closeFile($currentFile) : void
+    {
+        if (is_resource($currentFile)) {
+            fclose($currentFile);
+        }
+    }
+
+    private static function createNewFile($fileCount, $headers)
+    {
+        $fileName = self::OUTPUT_DIR . "/split_{$fileCount}.csv";
+        echo "Creating file $fileName\n";
+        $file = fopen($fileName, 'w');
+
+        if (!$file) {
+            throw new \RuntimeException("Failed to create new file: {$fileName}");
+        }
+
+        fputcsv($file, $headers);
+        return $file;
+    }
+}
+
 
 
 $csv = "data/Enquiry Table 1.csv";
-$test = CsvReader::readCSV($csv, 1);
+//$test = CsvReader::execute($csv, 1);
 // don't use json_encode, too confusing to tell where encoding issues lie
-echo var_dump($test);
+//echo var_dump($test);
+
+//$csv = "data/Test Export Edited.csv";
+CsvSplitter::execute($csv, 1, 5);
